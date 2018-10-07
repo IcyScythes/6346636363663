224989abc8e75de18dc62c89afcde2f7999e4a93
IcyScythes <Clashofraiding@gmail.com>
2018-10-07 10:08:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+const discord = require('discord.js');
+const roblox = require('roblox-js');
+const client = new discord.Client();
+const token = "NDk4MTMwMDM3NTE4NzYyMDE0.DpqVJQ.d3YXQjqJJ0cM1ROUclgJazrXM58";
+
+roblox.login({username: "FrostyEmpire_1", password: "Injustice33"}).then((success) => {
+
+}).catch(() => {console.log("Sorry, it failed.");});
+
+
+client.on("ready", () => {
+  client.user.setGame(`Making HL3`);
+  console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
+});
+
+client.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  let user = member.user
+  console.log(`${user.tag} joined ${guild}`)
+});
+
+client.on('guildMemberRemove', member => {
+  let guild = member.guild;
+  let user = member.user
+  console.log(`${user.tag} left ${guild}`)
+});
+
+var prefix = '!';
+var groupId = 2763062;
+var maximumRank = 16;
+
+function isCommand(command, message){
+	var command = command.toLowerCase();
+	var content = message.content.toLowerCase();
+	return content.startsWith(prefix + command);
+}
+
+client.on('message', (message) => {
+	if (message.author.bot) return; // Dont answer yourself.
+    var args = message.content.split(/[ ]+/)
+    
+    if(isCommand('Promote', message)){
+    	var username = args[1]
+    	if (username){
+    		message.channel.send(`Checking ROBLOX for ${username}`)
+    		roblox.getIdFromUsername(username)
+			.then(function(id){
+				roblox.getRankInGroup(groupId, id)
+				.then(function(rank){
+					if(maximumRank <= rank){
+						message.channel.send(`${id} is rank ${rank} and not promotable.`)
+					} else {
+						message.channel.send(`${id} is rank ${rank} and promotable.`)
+						roblox.promote(groupId, id)
+						.then(function(roles){
+							message.channel.send(`Promoted from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
+						}).catch(function(err){
+							message.channel.send("Failed to promote.")
+						});
+					}
+				}).catch(function(err){
+					message.channel.send("Couldn't get him in the group.")
+				});
+			}).catch(function(err){ 
+				message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+			});
+    	} else {
+    		message.channel.send("Please enter a username.")
+    	}
+    	return;
+    }
+});
+
+function pluck(array){
+    return array.map(function(item) { return item['name']; })
+}
+
+function hasRole(members, role){
+    if(pluck(members.roles).includes(role)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isAdmin(message){
+	if(
+		hasRole(message.member,"Ranker")  
+		){
+
+		return true;
+	} else {
+		return false;
+	}
+}
+client.on('message', (message) => {
+
+if (isAdmin(message)){
+console.log('Is an admin!')
+}
+
+})
+
+client.login(token);
