4868c32039ed8fd54a26872c1c63d450e87f6c43
IcyScythes <Clashofraiding@gmail.com>
2018-10-13 22:15:33 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,37 +24,6 @@ client.on('guildMemberRemove', member => {
   console.log(`${user.tag} left ${guild}`)
 });
 
-function pluck(array){
-    return array.map(function(item) { return item['name']; })
-}
-
-function hasRole(members, role){
-    if(pluck(members.roles).includes(role)){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isAdmin(message){
-	if(
-		hasRole(message.member,"Ranker") || 
-		hasRole(message.member,"Generalissimus") 
-		){
-
-		return true;
-	} else {
-		return false;
-	}
-}
-client.on('message', (message) => {
-
-if (isAdmin(message)){
-console.log('Is an admin!')
-}
-
-});
-
 var prefix = ':';
 var groupId = 2763062;
 var maximumRank = 16;
@@ -129,7 +98,37 @@ client.on('message', (message) => {
     	}
     	return;
     }
+	function pluck(array){
+    return array.map(function(item) { return item['name']; })
+}
+
+function hasRole(members, role){
+    if(pluck(members.roles).includes(role)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isAdmin(message){
+	if(
+		hasRole(message.member,"Ranker")
+		){
+
+		return true;
+	} else {
+		return false;
+	}
+}
+
+client.on('message', (message) => {
+
+if (isAdmin(message)){
+console.log('Is an admin!')
+}
 });
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
